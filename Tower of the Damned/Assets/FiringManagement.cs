6e9ad2e35faeba831222a8d10d7347ac29189eb9
Markus <mark5472@h-u.dk>
2018-11-20 16:15:09 +0100

    Test 1 edit og Bue script og Arrrow prefab

---
+++ Tower of the Damned/Assets/FiringManagement.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FiringManagement : MonoBehaviour {
+    public GameObject Arrow;
+    public string Firekey = "Fire1";
+
+    [Range(1f,10f)]
+    public float ForceMod = 5f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown(Firekey))
+        {
+            Fire();
+        }
+	}
+
+    private void Fire()
+    {
+        GameObject temp;
+        temp = Instantiate(Arrow, transform.position + transform.forward, transform.rotation * Arrow.transform.rotation);
+        temp.GetComponent<Rigidbody>().AddForce(transform.forward * ForceMod, ForceMode.Impulse);
+    }
+}
