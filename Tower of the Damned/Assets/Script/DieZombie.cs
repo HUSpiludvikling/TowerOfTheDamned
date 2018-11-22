2c9cfa22d7e3bfa6978fff801235c278abeebe28
Markus <mark5472@h-u.dk>
2018-11-22 15:08:54 +0100

    Zombie die, Arrow Collider Destroy
    Jeg added to scripts

---
+++ Tower of the Damned/Assets/Script/DieZombie.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DieZombie : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<Health>().DeathEvent.AddListener(Die);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void Die()
+    {
+        Destroy(gameObject);
+    }
+
+
+}
