1731a23d687045c706dad55d1e29449d590eede4
KrisDG <Kris182n@h-u.dk>
2018-11-22 15:57:43 +0100

    Edit til ZombieDestination
    Nu går zombien mod dig hvis man er indefor "MinDistanceToPlayer"

--- Tower of the Damned/Assets/Script/SetZombieDestination.cs
+++ Tower of the Damned/Assets/Script/SetZombieDestination.cs
@@ -4,16 +4,33 @@ using UnityEngine;
 using UnityEngine.AI;
 
 public class SetZombieDestination : MonoBehaviour {
+    Vector3 Destination;
+    Transform PlayerTrans;
 
+    [Range (1f,10f)]
+    [SerializeField]
+    float MinDistanceToPlayer;
+    NavMeshAgent Agent;
+    
 	// Use this for initialization
 	void Start () {
-        Vector3 Destination = GameObject.FindGameObjectWithTag("Tower").transform.position;
+        Destination = GameObject.FindGameObjectWithTag("Tower").transform.position;
+        Agent = GetComponent<NavMeshAgent>();
+        Agent.SetDestination(Destination);
 
-        GetComponent<NavMeshAgent>().SetDestination(Destination);
+        PlayerTrans = GameObject.FindGameObjectWithTag("Player").transform;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        if (Vector3.Distance(transform.position, PlayerTrans.position) <= MinDistanceToPlayer)
+        {
+            Agent.SetDestination(PlayerTrans.position);
+
+        }
+        else
+        {
+            Agent.SetDestination(Destination);
+        }
 	}
 }
