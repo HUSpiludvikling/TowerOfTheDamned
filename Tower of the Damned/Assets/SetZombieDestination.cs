48c1339412e14434e2cf34381d24ca2d92e720eb
KrisDG <Kris182n@h-u.dk>
2018-11-22 15:19:22 +0100

    Nav Scene, Zombie Nav
    Jeg har lavet en scene til mit Nav arbejde, og jeg har lavet At zombierne går mod Tower.

---
+++ Tower of the Damned/Assets/SetZombieDestination.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class SetZombieDestination : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Vector3 Destination = GameObject.FindGameObjectWithTag("Tower").transform.position;
+
+        GetComponent<NavMeshAgent>().SetDestination(Destination);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
