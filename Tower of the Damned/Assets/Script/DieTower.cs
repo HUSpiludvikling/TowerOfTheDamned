ea6fb23f4e3cdb7b264660757e574d553da25cbd
KrisDG <Kris182n@h-u.dk>
2018-12-06 15:26:03 +0100

    Added "Die Tower" script, og added Lose Scene
    jeg har lavet et script der gør at når tower dør loader den en scene, og så added jeg den scene.

---
+++ Tower of the Damned/Assets/Script/DieTower.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DieTower : MonoBehaviour {
+
+    [SerializeField]
+    string SceneName = "Lose Scene";
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<Health>().DeathEvent.AddListener(Die);
+	}
+
+    void Die()
+    {
+        SceneManager.LoadScene(SceneName);
+    }
+}
