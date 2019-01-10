e93948b7e93f3be0ff175a80efd98ca7e52594b6
Markus <mark5472@h-u.dk>
2019-01-10 17:12:09 +0100

    Basic ResourceCollection scipt

---
+++ Tower of the Damned/Assets/Script/ResourceCollection.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourceCollection : MonoBehaviour {
+    public int Lumber;
+    public int Stone;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void AddLumber(int amount)
+    {
+        Lumber += amount;
+    }
+    public void AddStone(int amount)
+    {
+        Stone += amount;
+    }
+}
