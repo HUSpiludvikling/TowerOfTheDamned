750816f658cb3d7e7a204bb2766db037dc1faab5
Markus <mark5472@h-u.dk>
2019-01-15 16:59:40 +0100

    HealthBar scipt til player

---
+++ Tower of the Damned/Assets/Script/HealthBarPlayer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBarPlayer : MonoBehaviour {
+
+    public Health healthScript;
+
+    private Image barSprite;
+
+	// Use this for initialization
+	void Start () {
+        barSprite = GetComponent<Image>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float percent = (healthScript.currentHP / healthScript.MaxHP);
+        barSprite.fillAmount = percent;
+	}
+}
