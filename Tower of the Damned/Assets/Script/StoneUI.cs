46fa6cfdfa9b3f56d214733561612eee1b0f6802
Markus <mark5472@h-u.dk>
2019-01-22 15:44:50 +0100

    StoneUI script

---
+++ Tower of the Damned/Assets/Script/StoneUI.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StoneUI : MonoBehaviour {
+
+    [SerializeField]
+    private ResourceCollection resourceCollection;
+
+    Text Text;
+
+    private void Awake()
+    {
+        if (resourceCollection == null)
+        {
+            resourceCollection = GameObject.FindGameObjectWithTag("Player").GetComponent<ResourceCollection>();
+        }
+        Text = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Text.text = "Stone:" + resourceCollection.Stone;
+    }
+
+}
