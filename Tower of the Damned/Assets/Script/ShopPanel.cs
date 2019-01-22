6b003c38d13ea053f1bac81e55dad73144cf0283
Markus <mark5472@h-u.dk>
2019-01-22 16:58:44 +0100

    Shop Script

---
+++ Tower of the Damned/Assets/Script/ShopPanel.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShopPanel : MonoBehaviour {
+
+    //public bool CanActivte = false;
+
+    private Image img;
+
+
+	// Use this for initialization
+	void Start () {
+        img = GetComponent<Image>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+    public void SetStateRecursive(bool value)
+    {
+        img.enabled = value;
+        foreach (Transform item in transform)
+        {
+            item.gameObject.SetActive(value);
+        }
+    }
+}
---
+++ Tower of the Damned/Assets/ShopActivator.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopActivator : MonoBehaviour {
+
+    [SerializeField]
+    private ShopPanel shopPanel;
+
+    
+
+	// Use this for initialization
+	void Start () {
+        shopPanel = GetComponentInChildren<ShopPanel>();
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Shop"))
+        {
+            shopPanel.SetStateRecursive(true);
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Shop"))
+        {
+            shopPanel.SetStateRecursive(false);
+        }
+    }
+}
