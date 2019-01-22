d170b95b3814930bb7c789a822f1c7c6a2590a5c
Markus <mark5472@h-u.dk>
2019-01-22 15:20:46 +0100

    Arrow edit, LumberUI script
    Arrow skulle bare ændre noget med tags og lavde også et nyt script

---
+++ Tower of the Damned/Assets/Script/LumberUI.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LumberUI : MonoBehaviour {
+
+    [SerializeField]
+    private ResourceCollection resourceCollection;
+
+    Text Text;
+
+    private void Awake()
+    {
+        if (resourceCollection == null)
+        {
+            resourceCollection = GameObject.FindGameObjectWithTag("Player").GetComponent<ResourceCollection>();
+        }
+        Text = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        Text.text = "Lumber:" + resourceCollection.Lumber;
+	}
+
+}
